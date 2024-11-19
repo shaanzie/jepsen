5fb04970bd2ce9598f609486dfa58f8f076a0737
shaanzie <ishaanlagwankar@gmail.com>
2024-11-19 15:36:34 -0500

    Added cockroachdb.repcl

--- cockroachdb-repcl/test/cockroachdb_repcl/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cockroachdb-repcl.core-test
-  (:require [clojure.test :refer :all]
-            [cockroachdb-repcl.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
